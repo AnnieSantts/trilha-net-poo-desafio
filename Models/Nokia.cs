619e52897fbb060c33beec8a32b94026ce90088e
Annie Santana <annie.santtana@gmail.com>
2024-01-07 15:39:16 -0300

    implementacao nos testes em program.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -5,10 +5,10 @@ namespace DesafioPOO.Models
     {
 
         //construtor por herança
-        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
-        {
-
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria){
+            
         }
+        
 
         // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
--- Program.cs
+++ Program.cs
@@ -2,15 +2,16 @@
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
 
-Nokia n1 = new Nokia("(11)123458", "x12gb","12bhj36576", 250 );
+Nokia n1 = new Nokia("(11)9 1234-9858", "x12gb","12bhj36576", 250 );
 n1.InstalarAplicativo("Candy Crush");
 n1.ReceberLigacao();
 n1.Ligar();
-n1.Numero = "123456789";
+Console.WriteLine("Número atual: "+ n1.Numero);
+n1.Numero = "(11)9 2345-6789";
 Console.WriteLine("Número alterado para: "+ n1.Numero);
 
 
-Iphone a1 = new Iphone("(12)123458", "13 Pro Max","128956537", 500 );
+Iphone a1 = new Iphone("(12)9 1234-5865", "13 Pro Max","128956537", 500 );
 a1.InstalarAplicativo("Linkedin");
 a1.ReceberLigacao();
 a1.Ligar();
